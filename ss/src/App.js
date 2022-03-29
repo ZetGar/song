74acdb73eba701cd88522704bf2f06bd6e5ab6f7
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-03-29 23:14:06 +0900

    Update App.js

--- ss/src/App.js
+++ ss/src/App.js
@@ -1,10 +1,29 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 
 function App() {
+  // var data = "안녕하세요";
+  let posts = "강남 고기 맛집";
+  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집"]);
+
   return (
     <div className="App">
-      <div></div>
+      <div className="black-nav">
+        <h1>개발 blog</h1>
+        {/* <div>여기에 데이터바인딩?</div> */}
+        {/* <div>{data}</div> */}
+      </div>
+
+      <div className="list">
+        <h3> {글제목[0]} </h3>
+        <p> 2월 17일 발행 </p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p> 2월 17일 발행 </p>
+        <hr />
+      </div>
     </div>
   );
 }
