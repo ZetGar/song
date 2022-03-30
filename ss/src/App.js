2f7e107f4b27d28f5bf57c1ce77355da63ea8734
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-03-30 19:51:25 +0900

    Update App.js

--- ss/src/App.js
+++ ss/src/App.js
@@ -4,7 +4,11 @@ import "./App.scss";
 function App() {
   // var data = "안녕하세요";
   let posts = "강남 고기 맛집";
-  let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집"]);
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "축구는 왜 하니",
+  ]);
 
   return (
     <div className="App">
@@ -21,6 +25,11 @@ function App() {
       </div>
       <div className="list">
         <h3> {글제목[1]} </h3>
+        <p> 2월 17일 발행 </p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[2]} </h3>
         <p> 2월 17일 발행 </p>
         <hr />
       </div>
