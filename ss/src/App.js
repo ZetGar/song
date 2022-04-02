9eaa6235f519756425fc1f27202805ddada05838
ZetGar <88043986+ZetGar@users.noreply.github.com>
2022-04-02 18:07:54 +0900

    Update App.js
    강의2. 클릭시 숫자 변경 + 숙제 버튼클릭 시 제목 변경

--- ss/src/App.js
+++ ss/src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 
@@ -10,6 +12,8 @@ function App() {
     "축구는 왜 하니",
   ]);
 
+  let [따봉, 따봉변경] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -18,8 +22,25 @@ function App() {
         {/* <div>{data}</div> */}
       </div>
 
+      <button
+        onClick={() => {
+          글제목변경(["여자 코트 추천", "강남 우동 맛집", "축구는 왜 하니"]);
+        }}
+      >
+        버튼
+      </button>
       <div className="list">
-        <h3> {글제목[0]} </h3>
+        <h3>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>
+          {따봉}
+        </h3>
         <p> 2월 17일 발행 </p>
         <hr />
       </div>
