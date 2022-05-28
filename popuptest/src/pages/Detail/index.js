408b7e75727f0c7aadce80af7776dea19dd138c2
ZetGar <awesde10@gmail.com>
2022-05-28 20:54:52 +0900

    Update index.js

--- popuptest/src/pages/Detail/index.js
+++ popuptest/src/pages/Detail/index.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { DATA } from "./../../data/";
 
@@ -9,10 +10,39 @@ const Detail = () => {
 
   let num = findpdt.id + 1;
 
+  let [close, setClose] = useState(true);
+
+  useEffect(() => {
+    // html 렌더링 후에 동작함
+    // setTimeout(() => {
+    //   setClose(false);
+    // }, 2000);
+  });
+
+  let [count, setCount] = useState(0);
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
+          {/* <div className="alert alert-warning">2초 이내 구매시 할인</div> */}
+          {/* {close == true ? (
+            <div className="alert alert-warning">2초 이내 구매시 할인</div>
+          ) : null} */}
+
+          {/* {close == true ? (
+            <div className="popup">
+              <div></div>
+              <button
+                onClick={() => {
+                  setClose(false);
+                }}
+              >
+                닫기
+              </button>
+            </div>
+          ) : null} */}
+
           <img
             src={"https://codingapple1.github.io/shop/shoes" + num + ".jpg"}
           />
@@ -29,3 +59,11 @@ const Detail = () => {
 };
 
 export default Detail;
+
+// 과거에 쓰던 방식
+
+// class Detail2 extends React.Component {
+//   componentDidMount() {}
+//   componentDidUpdate() {}
+//   componentWillUnmount() {}
+// }
